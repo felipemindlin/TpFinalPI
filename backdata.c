04033a5d6ea7f9da98d78db0dc949d3dc8214a1c
Felipe <fmindlin@itba.edu.ar>
2022-07-04 20:54:33 -0300

    corregi: 1 strok de mas y  faltaban un par de atoi para convertir strings a int

--- backdata.c
+++ backdata.c
@@ -4,37 +4,29 @@
 
 void newData(sensorsADT sens, char * string){
     char * token;
-    token = strtok(string, DELIM);
 
-    token = strtok(NULL, DELIM); // año
-    int year = atoi(token);
+    token = strtok(string, DELIM); 
+    int year = atoi(token);// año
 
-    token = strtok(NULL, DELIM); // mes // REVISAR SI HACEMOS EL QUERY 4 O NO
-    char * month = token;
+    token = strtok(NULL, DELIM); 
+    char * month = token; // mes 
 
-    token = strtok(NULL, DELIM); // mDate
-    int day = token;
+    token = strtok(NULL, DELIM); 
+    int day = atoi(token); // mDate
 
-    token = strtok(NULL, DELIM); // Day // HAY QUE VER SI LO HACEMOS ENUM O NO, ESO CAMBIARIA EL TIPO DE DATO
-    int weekDay = token;
+    token = strtok(NULL, DELIM); 
+    int weekDay = atoi(token); // Day // HAY QUE VER SI LO HACEMOS ENUM O NO, ESO CAMBIARIA EL TIPO DE DATO
 
     token = strtok(NULL, DELIM); // sensor_id
-    int sensor = token;
+    int sensor = atoi(token);
     
-    token = strtok(NULL, DELIM); // time
-    int time = token;
+    token = strtok(NULL, DELIM); 
+    int time = atoi(token); // time
 
-    token = strtok(NULL, DELIM); // hourly_counts (no se que es)
-    int hCounts = token;
+    token = strtok(NULL, DELIM); 
+    int hCounts = atoi(token); // hourly_counts (no se que es)
 
-
-    
-
-
-
-
-
-
+    //FALTA LLAMAR A LAS FUNCIONES QUE AGREGUEN LA INFO AL TAD
 }
 
     
