b47e19b834a67bb933caed1837a4f38d08df5ef6
sant9s <sgalarraga@itba.edu.ar>
2022-07-04 20:09:46 -0300

    Added function newData

--- backdata.c
+++ backdata.c
@@ -1,1 +1,19 @@
 #include "sensorsADT.h"
+
+void newData(sensorsADT sens, char * string){
+    char * token;
+    token = strtok(string, DELIM);
+
+    token = strtok(NULL, DELIM);
+    
+
+
+
+
+
+
+
+
+    
+
+}
