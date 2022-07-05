1b367f520e32f49c193c0e544cef7f442db1f440
Felipe <fmindlin@itba.edu.ar>
2022-07-05 20:18:07 -0300

    cambios varios entre todos vol 6

--- backdata.c
+++ backdata.c
@@ -59,31 +59,23 @@ void addReading(sensorsADT data, char * string){
 
 int days(char * token){
     int n=2; // comparamos los primeros dos caracteres para ver que dia de la semana es 
-    if(strncmp(token, "Mo", n)==0) return Mon;
-    if(strncmp(token, "Tu", n)==0) return Tue;
-    if(strncmp(token, "We", n)==0) return Wed;
-    if(strncmp(token, "Th", n)==0) return Thur;
-    if(strncmp(token, "Fr", n)==0) return Fri;
-    if(strncmp(token, "Sa", n)==0) return Sat;
-    if(strncmp(token, "Su", n)==0) return Sun;
+    char *weekDay[] = {"Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"};
+    for(int i = 0; i < (sizeof(weekDay)/sizeof(weekDay[0])); i++){
+        if(strncmp(token, weekDay[i], n)==0){
+            return i;
+        }
+    }
     return -1;
 }
 
 int months(char * token){
    int n = 3; //para comparar primeros 3 caracteres del mes (diferenciar march-may, june-july)
-   if(strncmp(token, "Jan", n)==0) return Jan;
-   if(strncmp(token, "Feb", n)==0) return Feb;
-   if(strncmp(token, "Mar", n)==0) return Mar;
-   if(strncmp(token, "Apr", n)==0) return Apr;
-   if(strncmp(token, "May", n)==0) return May;
-   if(strncmp(token, "Jun", n)==0) return Jun;
-   if(strncmp(token, "Jul", n)==0) return Jul;
-   if(strncmp(token, "Aug", n)==0) return Aug;
-   if(strncmp(token, "Sep", n)==0) return Sep;
-   if(strncmp(token, "Oct", n)==0) return Oct;
-   if(strncmp(token, "Nov", n)==0) return Nov;
-   if(strncmp(token, "Dec", n)==0) return Dec;
-   return -1;
+   char * monthsVec[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+   int i=0; // n letras necesarias para diferenciar los meses
+   while(strncmp(token, monthsVec[i], n) != 0){
+        i++;
+   }
+   return i + 1;
 }   
 
 int isActive(sensorsADT data, int id){
