17d425c6844fd3ac93b960f1921831038666a28d
Felipe <fmindlin@itba.edu.ar>
2022-07-06 16:44:59 -0300

    FUNCIONA

--- sensorsADT.h
+++ sensorsADT.h
@@ -1,12 +1,157 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <assert.h>
 #include <errno.h>
 
 #define DELIM ";"
-#define ZERO 2009
-#define IDS 85
+#define MIN_YEAR 2009
+#define IDS 86
 #define DAYS 7
-
+#define ACTUAL_YEAR 2022
 typedef struct sensorsCDT * sensorsADT;
 
+/*
+* funcion que ordena de manera eficiente para devolver en el orden pedidio para el Query 1
+*/
+void orderQ1(sensorsADT data);
+
+/*
+* funcion que ordena de manera eficiente para devolver en el orden pedidio para el Query 4
+*/
+void orderQ4(sensorsADT data); 
+
+/*
+* Crea un TAD nuevo vacío. Recibe el año míninimo y máximo de mediciones a tener en cuenta. 
+* (Útiles para el query 3)
+*/
+sensorsADT newSensorsADT(size_t minYear, size_t maxYear);
+
+/*
+* Recibe el TAD, el año de mediciones y la medición particular.
+* Suma en el vector years al total de mediciones por año la medición recibida.
+*/
+void newyear(sensorsADT data, int year, int hCounts);
+
+/*
+* Recibe el TAD, nombre del día, hora y número de día de medición, y
+* retorna 1 si no pudo guardar en memoria, 0 en caso contrario.
+* Si guarda en memoria, suma al total del día.
+* También si time está entre 0 y 6 (horas): suma en day en vector days
+* Si time está entre 18 y 23 (horas): suma en night en vector days
+*/
+int newDay(sensorsADT data, char * nameday, int time, int hCounts, int weekDay);
+
+/*
+* Recibe el TAD, año, número y día de mes, nombre y numero del día de semana,
+* un id de sensor, una hora, una medición y un flag indicando el estado del sensor.
+* Llama a newID.
+* Si el flag está prendido, llama a newDay y si pudo guardar en memoria, llama a newyear, si no retorna 1.
+* En caso de poder guardar todo, retorna 0, indicando que no hubo problemas en memoria.
+*/
+int newReading(sensorsADT data, int year, int numMonth, int monthDay, char * nameday, int weekDay, int id, int time, int hCounts, int * status);
+
+/*
+* Recibe el TAD, un id de sensor, una medición, un año, un mes, un día del mes y una hora.
+* En el vector de sensores, en el sensor particular, suma al total de mediciones.
+* Si el año esta dentro del intervalo de año minímo y máximo, agrega datos de fecha en
+* la cantidad maxima de mediciones del sensor.
+*/
+void newID(sensorsADT data, int num_id, int hCounts, int year, int month, int monthDay, int time);
+
+/* 
+* Recibe TAD, id, nombre y estado de sensor.
+* Copia nombre en estructura, verifica si pudo reservar memoria, si no pudo retorna 1.
+* Si pudo, agrega el estado del sensor, luego retorna 0.
+*/
+int newSensor(sensorsADT data, size_t id, char * name, char status);
+
+/*
+* Retorna 1 si el sensor esta activo, 0 si no.
+*/
+int isActive(sensorsADT data, int id);  
+
+/*
+* Recibe TAD y libera la memoria reservada para los nombres
+* en vector de días y vector de sensores.
+*/
+void freeALL(sensorsADT data);
+
+/*
+* Recibe el TAD y linea del archivo.
+* Interpreta los datos de la linea y llama a la función newSensor.
+* Retorna el valor que retorne newSensor. 1 si no pudo reservar memoria, 0 en caso contrario.
+*/
+int addSensor(sensorsADT data, char * string);
+
+/*
+* Recibe el TAD, línea del archivo, minimo y maximo de años.
+* Incializa en -1 las variables month, monthDay y en 0 un flag.
+* Interpreta la linea y guarda en variables los datos de la medición.
+* Si el año que lee de la línea está entre el mínimo y máximo de años, prende el flag.
+* Si el flag está prendido guarda en month y monthDay los datos de la medición.
+* Siempre guarda en weekDay, id, time, hCounts y status los datos de la medición.
+* LLama a newReading y retorna lo que devuelva la misma. 1 si no pudo reservar memoria y 0 si pudo.
+*/
+int addReading(sensorsADT data, char * string, int min, int max);
+
+/* 
+* Recibe el TAD y un idx y retorna el nombre del sensor
+*/
+char * getName(sensorsADT data, int idx);
+
+/* 
+* Recibe el TAD y un idx y retorna la cantidad de mediciones en un año
+*/
+size_t getTotal(sensorsADT data, int idx);
+
+/*
+*Devuelve la suma total de mediciones del año idx+MIN_YEAR.
+*/
+size_t getDataByYear(sensorsADT data, int idx);
+
+/* 
+* Recibe el TAD y un idx y retorna el nombre del dia
+*/
+char * getDayName(sensorsADT data, int idx);
+
+/* 
+* Recibe el TAD y un idx y retorna la cantidad de mediciones que obtuvo entre las 0 y 6(horas)
+*/
+size_t getDataDay(sensorsADT data, int idx);
+
+/* 
+* Recibe el TAD y un idx y retorna la cantidad de mediciones que obtuvo entre las 18 y 23(horas)
+*/
+size_t getDataNight(sensorsADT data, int idx);
+
+/* 
+* Recibe el TAD y un idx y retorna la cantidad de mediones que obtuvo en un dia
+*/
+size_t getDataTotal(sensorsADT data, int idx);
+
+/* 
+* Recibe el TAD y un idx y retorna la maxima medicion que obtuvo un sensor en una hora
+*/
+size_t getMax(sensorsADT data, int idx);
+
+/* 
+* Recibe el TAD y un idx y retorna la hora
+*/
+int getHour(sensorsADT data, int idx);
+
+/* 
+* Recibe el TAD y un idx y retorna el dia
+*/
+int getDay(sensorsADT data, int idx);
+
+/* 
+* Recibe el TAD y un idx y retorna el mes
+*/
+int getMonth(sensorsADT data, int idx);
+
+/* 
+* Recibe el TAD y un idx y retorna el año
+*/
+int getYear(sensorsADT data, int idx);
--- sensorsfront.c
+++ sensorsfront.c
@@ -1,11 +1,30 @@
 #include "sensorsADT.h"
 #define MAX_LEN 64 // reservamos 64 ya que analizando los parametros llegamos a que el maximo son 55 caracaracter rervamos mas por si cambian los id o hay mediciones absurdamente grandes.
-void memory(void);
-
+void memory();
+void wrongyear(void);
+int check(char *s);
 int main(int argc, char *argv[]){
-    if( argc < 2 || argc > 4 ) {
+    if( argc < 2 || argc > 5 ) {
         fprintf(stderr, "ERROR: cantidad de argumentos invalida\n");
         exit(1);
+    }
+    int min=MIN_YEAR;
+    int max=ACTUAL_YEAR;
+    if(argc==3){
+        if(check(argv[3])){
+            wrongyear();
+        }
+        min=atoi(argv[3]);
+    }
+    else if(argc==4){
+        if( check(argv[3]) || check(argv[4])){
+           wrongyear();
+        }
+        min=atoi(argv[3]);
+        max=atoi(argv[4]);
+        if(min>max){
+            wrongyear();
+        }
     }
     FILE * readings = fopen(argv[1], "r");     // abrimos las mediciones
     FILE * sensors = fopen(argv[2], "r");       // abrimos el archivo csv de los sensores
@@ -13,20 +32,20 @@ int main(int argc, char *argv[]){
         fprintf(stderr, "ERROR: archivos especificados no encontrados\n");
         exit(1);
     }
-    sensorsADT data = newsensorADT();         // creamos un nuevo ADT y checkeamos de tener memoria
+    sensorsADT data = newSensorsADT(min, max);         // creamos un nuevo ADT y checkeamos de tener memoria
     if(data==NULL){
         memory();
     }
     char * currentLine=malloc(MAX_LEN); 
     fgets(currentLine, MAX_LEN, sensors); // salteo el encabezado
     while(fgets(currentLine, MAX_LEN, sensors)){ //recorro todas las lineas 
-        if(addSensor(sensors, currentLine)){
+        if(addSensor(data, currentLine)){
             memory();
         }
     }
     fgets(currentLine, MAX_LEN, readings); // salteo el encabezado
     while(fgets(currentLine, MAX_LEN, readings)){
-        if(addReading(readings, currentLine)){
+        if(addReading(data, currentLine, min, max)){
             memory();
         }
     }
@@ -45,18 +64,50 @@ int main(int argc, char *argv[]){
     fputs("year;counts\n", query2);
     fputs("day;day_counts;night_counts;total_counts\n", query3);
     fputs("sensor;max_counts;hour;date\n", query4);
-    
-
-
-
+    int i=0;
+    orderQ1(data);
+    while(i<IDS){
+        char * aux=getName(data, i);
+        if(aux!=NULL){
+            fprintf(query1,"%s;%ld\n", aux, getTotal(data, i));
+        }
+        i++;
+    }
+    for(i=ACTUAL_YEAR-MIN_YEAR; i>=0; i--){
+        fprintf(query2, "%d;%ld\n", i+MIN_YEAR, getDataByYear(data, i)); 
+    }
+    for(i=0; i<DAYS; i++){
+        fprintf(query3, "%s;%ld;%ld;%ld\n", getDayName(data, i), getDataDay(data, i), getDataNight(data, i), getDataTotal(data, i)); 
+    }
+    orderQ4(data);
+    for(i=min-MIN_YEAR; i<max-MIN_YEAR; i++){
+        fprintf(query4, "%s;%ld;%d;%d/%d/%d\n",getName(data, i), getMax(data, i), getHour(data, i), getDay(data, i), getMonth(data, i), getYear(data, i));
+    }
     free(currentLine);
+    freeALL(data);
     fclose(query1);
     fclose(query2);
     fclose(query3);
     fclose(query4);
+    return 0;
 }
 
 void memory(){
     fprintf(stderr, "ERROR: memoria insuficiente\n");
     exit(1);
 }
+void wrongyear(void){
+    fprintf(stderr, "ERROR: Paramtros de años invalidos\n");
+    exit(1);
+}
+
+int check (char * s){
+    for(int i=0; s[i]; i++){
+         if(!isdigit(s[i])){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
