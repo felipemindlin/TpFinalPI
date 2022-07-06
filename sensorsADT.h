3590ecece061b9b7679394fbaa93fff0fcacf79b
Felipe <fmindlin@itba.edu.ar>
2022-07-05 21:57:50 -0300

    cambios varios entre todos vol 7

--- sensorsADT.h
+++ sensorsADT.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <errno.h>
 
 #define DELIM ";"
@@ -14,3 +15,23 @@ typedef struct sensorsCDT * sensorsADT;
 void orderQ1(sensorsADT data); // funcion que ordena de manera eficiente para devolver en el orden pedidio para el Query 1
 
 void orderQ4(sensorsADT data); // funcion que ordena de manera eficiente para devolver en el orden pedidio para el Query 4
+
+
+
+sensorsADT newSensorsADT(size_t minYear, size_t maxYear);
+
+void newyear(sensorsADT data, int year, int hCounts);
+
+int newDay(sensorsADT data, char * nameday, int time, int hCounts, int weekDay);
+
+int newReading(sensorsADT data, int year, int numMonth, int monthDay, char * nameday, int weekDay, int id, int time, int hCounts, int * status);
+
+void newID(sensorsADT data, int num_id, int hCounts, int year, int month, int monthDay, int time);
+
+int newSensor(sensorsADT data, size_t id, char * name, char status);
+
+
+void orderQ1(sensorsADT data);
+void orderQ4(sensorsADT data);
+
+void freeALL(sensorsADT data);
--- sensorsCDT.c
+++ sensorsCDT.c
@@ -35,36 +35,59 @@ sensorsADT newSensorsADT(size_t minYear, size_t maxYear){
     return new;
 }
 
-static int compareTotalPeople(id sens1, id sens2); // devuelve 1 si el id1 tiene que ir antes cuando lo ordenas o -1 si deberia ir el id2 antes
-static int compareMax(id sens1, id sensMax); // compara la siguiente cantidad de personas en una hora dada del sensor con el record maximo. Si lo supera devuelve -1, si no devuelve 1
+static int compareTotalPeople(id * sens1, id * sens2); // devuelve 1 si el id1 tiene que ir antes cuando lo ordenas o -1 si deberia ir el id2 antes
+static int compareMax(id * sens1, id * sensMax); // compara la siguiente cantidad de personas en una hora dada del sensor con el record maximo. Si lo supera devuelve -1, si no devuelve 1
 
+void newyear(sensorsADT data, int year, int hCounts){
+    data->years[year-MIN_YEAR]+=hCounts;
+}
 
-int newReading(sensorsADT data, int year, int numMonth, int day, char * nameday, int weekDay, int id, int time, int hCounts, int * status){
-    if(status){
-        newID(&data->ids, id, hCounts, year, numMonth, day, time) // recibe sensor, id
-        newday() 
-        newyear()
+int newDay(sensorsADT data, char * nameday, int time, int hCounts, int weekDay){
+    day aux = data->days[weekDay];
+    aux.name = strcpy(malloc(strlen(nameday) + 1), nameday);
+    if (aux.name == NULL){
+        return 1; //no pudo guardar en memoria, 1 para que tire error
+    }
+    
+    aux.total += hCounts; //siempre sumo al total
+    if( time >= 0 && time <= 6){ //sumo si entra en el rango horario del daycount
+        aux.day += hCounts;
+    } else if( time>=18 && time<24){ //idem pero con nightcount
+        aux.night += hCounts;
+    }
+    return 0;
+}
+
+int newReading(sensorsADT data, int year, int numMonth, int monthDay, char * nameday, int weekDay, int id, int time, int hCounts, int * status){
+        newID(data, id, hCounts, year, numMonth, monthDay, time); 
+        if(*status){
+            if(newDay(data, nameday, time, hCounts, weekDay)){
+                return 1;
+            }
+            newyear(data, year, hCounts);
+        }
+        return 0;
+}
+
+void newID(sensorsADT data, int num_id, int hCounts, int year, int month, int monthDay, int time){
+     id aux=data->ids[num_id-1];
+     aux.total += hCounts;
+     
+     if(year >= data->minYear && year <= data->maxYear){
+        if(aux.cant_max <= hCounts){
+            //if() definir si al ser iguales tomo otro criterio o no
+            aux.cant_max = hCounts;
+            aux.year = year;
+            aux.year = month;
+            aux.day = monthDay;
+            aux.hour = time;
+        }
     }
     
 }
 
-void newID(id * sensor, int num_id, int hCounts, int year, int month, int day, int time){
-     id aux=sensor[num_id-1];
-     
-     if(aux.cant_max <= hCounts){
-        if()
-        aux.cant_max = hCounts;
-        aux.year = year;
-        aux.year = month;
-        aux.day = day;
-        aux.hour = time;
-        
-        }
-    
-}
-
 int newSensor(sensorsADT data, size_t id, char * name, char status){
-    id aux=data->ids[id-1];
+    id aux = data->ids[id-1];
     aux->name=strcpy(malloc(strlen(name)+1), name);
     if(aux->name == NULL){
         return 1;
@@ -76,15 +99,15 @@ int newSensor(sensorsADT data, size_t id, char * name, char status){
 static int compareTotalPeople(id * sens1, id * sens2){
   int resp;
     if((resp=sens2->total - sens1->total)==0){
-        return strcmp(sens1->name, sens2->name)
+        return strcmp(sens1->name, sens2->name);
     }
     return resp;
 }
 
 static int compareMax(id * sens1, id * sens2){
     int resp;
-    if((resp=sen2->cant_max - sens1->cant_max)==0){
-        return strcmp(sens1->name, sens2->name)
+    if((resp=sens2->cant_max - sens1->cant_max)==0){
+        return strcmp(sens1->name, sens2->name);
     }
     return resp;
 }
@@ -94,4 +117,14 @@ void orderQ1(sensorsADT data){
 } 
 void orderQ4(sensorsADT data){
     qsort(data->ids, IDS, sizeof(id), (int (*) (const void *, const void *))compareMax);
+}
+
+void freeALL(sensorsADT data){
+    for(int i=0; i<IDS; i++){
+        free(data->ids[i]->name);
+    }
+    for(int i=0; i<DAYS; i++){
+        free(data->days[i]->name);
+    }
+    free(data);
 }
